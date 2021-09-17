2fab5bfe54d55816c95d868b626da519b0117c4d
Wang Qing <vincent.w.qing@gmail.com>
2021-09-17 16:02:08 -0400

    adding subject rerun-preproc

--- preproc/fmriprep_func_sub.sh
+++ preproc/fmriprep_func_sub.sh
@@ -8,15 +8,15 @@ DATA_DIR=${WD_DIR}/${DATA_NAME}
 BIDS_DIR=${DATA_DIR}_BIDS
 CODE_DIR=${WD_DIR}/interventionMDD/preproc
 
-CODE_SLURM=${CODE_DIR}/fmriprep_anat_sub.slurm
+CODE_SLURM=${CODE_DIR}/fmriprep_func_sub.slurm
 
 CON_IMG=${WD_DIR}/container_images/fmriprep_v20.2.0.simg
-LOG_DIR=${DATA_DIR}_fmriprep_anat.log
+LOG_DIR=${DATA_DIR}_fmriprep_func.log
 
 chmod +x ${CODE_SLURM}
-
 echo "Step1: starting fmriprep-SP!"
 
+# PPMI
 #SUB_ID=037S4028
 sbatch ${CODE_SLURM} ${DATA_NAME} ${CON_IMG} ${SUB_ID} >> ${LOG_DIR}
 echo "Submission finished!"
